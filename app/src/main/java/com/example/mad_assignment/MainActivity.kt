81a491d5a73f84471a0827e75c8e5b1d2b380b87
Jack <dongwj-wm21@student.tarc.edu.my>
2024-05-20 00:29:05 +0800

    20/5/2024 updated some navigations and pass data

--- app/src/main/java/com/example/mad_assignment/MainActivity.kt
+++ app/src/main/java/com/example/mad_assignment/MainActivity.kt
@@ -19,8 +19,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
         abc = AppBarConfiguration(
             setOf(
                 R.id.property_Home,
