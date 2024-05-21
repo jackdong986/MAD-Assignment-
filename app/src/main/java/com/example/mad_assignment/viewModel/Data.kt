f5b33a302ee5832fd44bf1de8874e0a87771ab62
Fu Lim <fulim1130@gmail.com>
2024-05-21 19:20:27 +0800

    update spinner State for property Update

--- app/src/main/java/com/example/mad_assignment/viewModel/Data.kt
+++ app/src/main/java/com/example/mad_assignment/viewModel/Data.kt
@@ -2,6 +2,7 @@ package com.example.mad_assignment.viewModel
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import androidx.core.content.ContextCompat.getString
 import com.example.mad_assignment.R
 import com.example.mad_assignment.util.toBlob
 import com.google.firebase.Firebase
@@ -56,12 +57,13 @@ val RENTING = Firebase.firestore.collection("renting")
 fun RESTORE(ctx: Context){
     fun getBlob(res: Int) = BitmapFactory.decodeResource(ctx.resources, res).toBlob()
 
+    val statesArray: Array<String> = ctx.resources.getStringArray(R.array.states)
     val properties = listOf(
-        Property("P001", "House 1", 19.00, getBlob(R.raw.house1), "123 Street", "Kuala Lumpur", "Kuala Lumpur", 2, 3, "This is a house", "H001"),
-        Property("P002", "House 2", 109.00, getBlob(R.raw.house2), "123 Street", "Kuala Lumpur", "Selangor", 2, 3, "This is a house", "H001"),
-        Property("P003", "House 3", 119.00, getBlob(R.raw.house3), "123 Street", "Kuala Lumpur", "Kuala Lumpur", 2, 3, "This is a house", "H001"),
-        Property("P004", "House 4", 29.00, getBlob(R.raw.house4), "123 Street", "Kuala Lumpur", "Penang", 2, 3, "This is a house", "H001"),
-        Property("P005", "House 5", 9.00, getBlob(R.raw.house5), "123 Street", "Kuala Lumpur", "Kuala Lumpur", 2, 3, "This is a house", "H001"),
+        Property("P001", "House 1", 19.00, getBlob(R.raw.house1), "123 Street", "Kuala Lumpur", statesArray[1], 2, 3, "This is a house", "H001"),
+        Property("P002", "House 2", 109.00, getBlob(R.raw.house2), "123 Street", "Kuala Lumpur", statesArray[2], 2, 3, "This is a house", "H001"),
+        Property("P003", "House 3", 119.00, getBlob(R.raw.house3), "123 Street", "Kuala Lumpur", statesArray[1], 2, 3, "This is a house", "H001"),
+        Property("P004", "House 4", 29.00, getBlob(R.raw.house4), "123 Street", "Kuala Lumpur", statesArray[3], 2, 3, "This is a house", "H001"),
+        Property("P005", "House 5", 9.00, getBlob(R.raw.house5), "123 Street", "Kuala Lumpur", statesArray[1], 2, 3, "This is a house", "H001"),
 
     )
 
