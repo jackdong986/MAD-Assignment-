a97c68fa30b38a1571aced5cd1b6f0a092bc5c74
Fu Lim <fulim1130@gmail.com>
2024-05-21 19:40:24 +0800

    update empty fragment for profile and reservation

---
+++ app/src/main/java/com/example/mad_assignment/main/HostReservationFragment.kt
@@ -0,0 +1,39 @@
+package com.example.mad_assignment.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.SearchView
+import androidx.core.os.bundleOf
+import androidx.fragment.app.activityViewModels
+import com.example.mad_assignment.R
+import com.example.mad_assignment.databinding.FragmentPropertyHostBinding
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mad_assignment.adapter.PropertyHostAdapter
+import com.example.mad_assignment.databinding.FragmentHostReservationBinding
+import com.example.mad_assignment.viewModel.PropertyVM
+
+
+class HostReservationFragment : Fragment() {
+
+    private lateinit var binding: FragmentHostReservationBinding
+    private val nav by lazy { findNavController() }
+
+    private val propertyVM: PropertyVM by activityViewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentHostReservationBinding.inflate(inflater, container, false)
+
+
+
+
+        return binding.root
+    }
+
+
+}
